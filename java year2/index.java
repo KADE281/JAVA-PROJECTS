9911a0dd3b0aa75e9629dc65108bfbbee4afdbd7
KWIZERA David <kwizeradev1@gmail.com>
2026-02-07 23:13:50 +0200

    Create index.java in java year2 directory

---
+++ java year2/index.java
@@ -0,0 +1,16 @@
+class index{
+    public static void main(String[] args) {
+        
+      int A = 1;
+      int B = 2;
+      
+      if((A == 1) && (B == 2)){
+          System.out.print("Value1 is equal to value1");
+      }
+     else if(A == 1 || B ==1){
+          System.out.print("Value1 is 1 or value2 is 1");
+
+      }
+
+    }
+}
